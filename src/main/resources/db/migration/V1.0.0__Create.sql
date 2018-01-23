77e47ec819f33ccb5ceaa6a51013146e8cc19250
ta-satoh <starlightprayer@gmail.com>
2018-01-24 03:01:19 +0900

    flyway削除

--- src/main/resources/db/migration/V1.0.0__Create.sql
+++
@@ -1,8 +0,0 @@
-create table Movies (
-  MovieId serial
-  , title character varying(256)
-  , director character varying(64)
-  , runnningTime character varying(4)
-  , constraint Movies_PKC primary key (MovieId)
-) ;
-
