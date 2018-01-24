610393dff448c699b025b554614bb21ac242f3eb
ta-satoh <starlightprayer@gmail.com>
2018-01-24 13:29:35 +0900

    コメント追加

--- src/main/java/co/ie12/Ie12SampleMovie/service/MovieService.java
+++ src/main/java/co/ie12/Ie12SampleMovie/service/MovieService.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+/*
+ * 映画を参照するサービスクラス
+ */
 public class MovieService {
 
 }
