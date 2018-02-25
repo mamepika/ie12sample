88d463bbab06676191a04c3bf08f8a45e87c8a15
ta-satoh <starlightprayer@gmail.com>
2018-02-25 18:58:58 +0900

    add comment

--- src/main/java/co/ie12/Ie12SampleMovie/service/PriceCalculatorFactory.java
+++ src/main/java/co/ie12/Ie12SampleMovie/service/PriceCalculatorFactory.java
@@ -2,8 +2,9 @@ package co.ie12.Ie12SampleMovie.service;
 
 import co.ie12.Ie12SampleMovie.entity.Schedule;
 
-
-
+/**
+ * 料金計算のファクトリークラス
+ */
 public class PriceCalculatorFactory {
 
     public static PriceCalculator create(Schedule schedule){
