eceada7ab5585b046b4219f126b9d20d2b98b1f0
ta-satoh <starlightprayer@gmail.com>
2018-02-25 22:25:03 +0900

    add late show class

---
+++ src/main/java/co/ie12/Ie12SampleMovie/service/LateShowPriceCalculator.java
@@ -0,0 +1,13 @@
+package co.ie12.Ie12SampleMovie.service;
+
+import java.math.BigDecimal;
+
+/**
+ * レイトショーの上映料金計算クラス
+ */
+public class LateShowPriceCalculator implements  PriceCalculator{
+    @Override
+    public BigDecimal calculatePrice() {
+        return null;
+    }
+}
